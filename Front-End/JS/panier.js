ff39035bcf7667a92906bb22c5e038228210b7de
Jonathan <42.jonathan.garcia@gmail.com>
2021-09-16 12:58:29 +0200

    ADD (JS) - Panier

---
+++ Front-End/JS/panier.js
@@ -0,0 +1,157 @@
+/**********
+
+*/
+/*
+
+**********/
+
+
+
+/**********
+    GET - API
+*/
+const getData = async function () {
+    let response = await fetch('http://localhost:3000/api/cameras')
+    let data = await response.json()
+    return data
+}
+/*
+    GET - API
+**********/
+
+
+/**********
+    PROMISE
+*/
+getData().then((data) => {
+
+
+    /**********
+        DISPLAY - Affichage des produits du panier
+    */
+    let tbody = document.querySelector('tbody')
+    let prix_total = null;
+
+    for (counter = 0; counter < localStorage.index; counter++)
+    {
+        if (localStorage.getItem("item-" + counter))
+        {
+            let new_tr = tbody.appendChild(document.createElement('tr'))
+
+                /***  Articles avec Datas  ***/
+                let new_td
+                item = JSON.parse(localStorage.getItem("item-" + counter))
+                new_td = new_tr.appendChild(document.createElement('td'))
+                new_td.textContent = item.name
+                new_td = new_tr.appendChild(document.createElement('td'))
+                new_td.textContent = item.objectif
+                new_td = new_tr.appendChild(document.createElement('td'))
+                new_td.textContent = item.quantite
+                new_td = new_tr.appendChild(document.createElement('td'))
+                new_td.textContent = item.prix
+                prix_total += item.prix * item.quantite
+
+                /***  Boutton Supprimer  ***/
+                let new_button = new_tr.appendChild(document.createElement('button'))
+                new_button.textContent = "Supprimer"
+                // new_button.classList = "p-1"
+                new_button.id = "suppButton_" + counter
+                
+                    /***  Boutton Supprimer - EVENT LISTENER ***/
+                    new_button.addEventListener('click', (event) => {
+                        new_tr.remove()
+                        localStorage.removeItem("item-" + new_button.id.replace(/\D/g,''))
+                        document.location.reload();
+                        // console.log(localStorage) //
+                    })
+
+            }
+    }
+    /*
+        DISPLAY - Affichage des produits du panier
+    **********/
+
+
+    /**********
+        DISPLAY - Prix Total (afficher)
+    */
+    document.querySelector("#prix-total").textContent = prix_total
+    /**********
+        DISPLAY - Prix Total (afficher)
+    */
+
+
+    /**********
+        BUTTON - Remove ALL panier
+    */
+    let panier_remove = document.querySelector("button#panier_remove")
+    panier_remove.addEventListener('click', (event) => { 
+        panier_remove.parentElement.querySelector('table').remove()
+        panier_remove.parentElement.querySelector("th#prix-total").remove()
+        localStorage.clear()
+    })
+    /*
+        BUTTON - Remove ALL panier
+    **********/
+    
+})
+/*
+    PROMISE
+**********/
+
+
+
+/**********
+    FUNCTION
+*/
+
+/*  replaceAt  */
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
+/*  onlyNumber  */
+function onlyNumber(id)
+{
+    var champ = document.getElementById(id);
+    while (champ.value.match(/[^0-9]/))
+    {
+        champ.value = champ.value.replace(/[^0-9]/,'');
+    }
+    console.log(champ.value)
+    if (champ.value[5]) {
+        let text = champ.value
+        // let text = new String("TEST")
+        // let text = champ.value
+        // console.log(text);
+        text = text.replaceAt(5, " ")
+        // console.log(text[0]);
+        champ.value = text 
+        // champ.value = "T"       
+    }
+}
+
+// function redirect() {
+//     window.location.href = "confirmation.html";
+// }
+
+/*
+    FUNCTION
+**********/
+
+const btn_envoyerForm = document.querySelector("#envoyerForm");
+btn_envoyerForm.addEventListener("click", () => {
+    var form = {}
+    form["nom"] = document.querySelector("#nom").value
+    form["prenom"] = document.querySelector("#prenom").value
+    form["adress"] = document.querySelector("#adress").value
+    form["ville"] = document.querySelector("#ville").value
+    form["cp"] = document.querySelector("#cp").value
+    form["email"] = document.querySelector("#email").value
+
+    localStorage.setItem("form", JSON.stringify(form))
+
+    // console.log(JSON.parse(localStorage.getItem("form")))
+
+})
+
